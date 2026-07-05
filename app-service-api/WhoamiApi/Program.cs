c3ac59147ad39762790585cdfc2769c8caa806ef
not-ed <43544402+not-ed@users.noreply.github.com>
2026-07-05 21:00:50 +0100

    Created stubbed AniListWatchlistService for most recent manga/anime seen
    A new Service has been added, along with a `/watchlist/current` endpoint, which will eventually be used for returning public activity associated to an AniList account that is configured against the whoami API. In this particular case, the most recent manga or anime series that the user in question has made progress on.
    
    The long-term goal is to have this data populated inside of SQL Server, and allow which titles are displayed/returned by the API to be curated. Until then, a hard-coded response has been added until the rest of this is implemented through Azure Functions.

--- app-service-api/WhoamiApi/Program.cs
+++ app-service-api/WhoamiApi/Program.cs
@@ -2,12 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using WhoamiApi;
 using WhoamiApi.Events;
 using WhoamiApi.Events.Github;
+using WhoamiApi.Watchlist;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<GithubEventsDatabaseContext>(options => options.UseSqlServer(ConnectionStringFactory.GetConnectionString(builder.Configuration)));
 builder.Services.AddTransient<EventsService>();
+builder.Services.AddTransient<AniListWatchlistService>();
 var app = builder.Build();
 
 app.MapGet("/activity", (EventsService e) => e.GetEvents());
+app.MapGet("/watchlist/current", (AniListWatchlistService e) => e.GetLatestTitle());
 
 app.Run();
---
+++ app-service-api/WhoamiApi/Watchlist/GetLatestAniListTitleResponse.cs
@@ -0,0 +1,8 @@
+namespace WhoamiApi.Watchlist;
+
+public class GetLatestAniListTitleResponse
+{
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public string Url { get; set; }
+}
