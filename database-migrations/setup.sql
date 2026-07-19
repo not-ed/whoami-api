0f3b973911c7356e721d173e18c72ac47a67d5cf
Edward Barton <43544402+not-ed@users.noreply.github.com>
2026-07-19 17:13:59 +0100

    Created anilist_titles and pending_anilist_approvals Tables in SQL Server
    2 tables have been added to `setup.sql`; `anilist_titles` and `pending_anilist_approvals`, which are used to store details such as title and a page URL for any manga/anime titles that have been read/watched by a configured AniList user. In order to allow a minimal degree of curation over which titles a user may want to display publicly through the whoami API, the latter table is used to store a one-time id for each new title that is discovered, and must be approved or denied before it can be returned by the API, which will be accomplished in a future App Service endpoint, or through a HTTP trigger-driven function.

--- database-migrations/setup.sql
+++ database-migrations/setup.sql
@@ -3,3 +3,21 @@ CREATE TABLE github_events(
     body json NOT NULL,
     created_at datetime2 NOT NULL
 );
+
+CREATE TABLE anilist_titles
+(
+    id bigint PRIMARY KEY NOT NULL,
+    name nvarchar(200) NOT NULL,
+    title_type varchar(5) NOT NULL,
+    url nvarchar(100) NOT NULL,
+    updated_at datetime2 NOT NULL,
+    approved bit DEFAULT 'FALSE'
+);
+
+CREATE TABLE pending_anilist_approvals(
+    id uniqueidentifier PRIMARY KEY NOT NULL DEFAULT NEWID(),
+    anilist_id bigint NOT NULL UNIQUE,
+    approval_prompt_sent bit DEFAULT 'FALSE',
+
+    CONSTRAINT fk_pending_anilist_approvals_anilist_titles FOREIGN KEY (anilist_id) REFERENCES anilist_titles(id) ON DELETE CASCADE
+);
